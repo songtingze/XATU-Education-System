7cb5f622434d493b0a2c44ef37b330ddd9b34f29
songtingze <18301135@bjtu.edu.cn>
2023-05-10 10:43:54 +0800

    部署云端redis，并实现远程连接 配置redis工具类

--- xatu-system/src/main/java/com/xatu/system/XatuSystemApplication.java
+++ xatu-system/src/main/java/com/xatu/system/XatuSystemApplication.java
@@ -3,9 +3,11 @@ package com.xatu.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.xatu"})
 @EnableEurekaClient  //此注解是用于在Eureka注册客户端使用
+//@ComponentScan()
 public class XatuSystemApplication {
 
     public static void main(String[] args) {
