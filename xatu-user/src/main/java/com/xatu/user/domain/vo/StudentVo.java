ed9d7661322867ff34543d93e24572332fd8e89c
Wang-Lei-2020 <60784777+Wang-Lei-2020@users.noreply.github.com>
2023-05-11 15:20:28 +0800

    实现登录注销功能，并使用StpUtil轻量化用户登录功能

--- xatu-user/src/main/java/com/xatu/user/domain/vo/StudentVo.java
+++ xatu-user/src/main/java/com/xatu/user/domain/vo/StudentVo.java
@@ -1,8 +1,26 @@
 package com.xatu.user.domain.vo;
 
+import com.xatu.user.domain.Student;
+import lombok.Data;
+
+
 /**
  * @author Wang Lei
  */
-public class StudentVo {
+@Data
+public class StudentVo extends Student {
+    private String userToken;
 
+    public StudentVo (Student student) {
+        this.setId(student.getId());
+        this.setName(student.getName());
+        this.setNumber(student.getNumber());
+        this.setPassword(student.getPassword());
+        this.setSchool(student.getSchool());
+        this.setEnrollmentTime(student.getEnrollmentTime());
+        this.setAge(student.getAge());
+        this.setHousehold(student.getHousehold());
+        this.setCreateTime(student.getCreateTime());
+        this.setUpdateTime(student.getUpdateTime());
+    }
 }
--- xatu-user/src/main/java/com/xatu/user/service/StudentService.java
+++ xatu-user/src/main/java/com/xatu/user/service/StudentService.java
@@ -1,8 +1,16 @@
 package com.xatu.user.service;
 
+import com.xatu.user.domain.Student;
+
 /**
  * @author Wang Lei
  */
 public interface StudentService {
-
+    /**
+     * 登录功能
+     * @param number 学生学号
+     * @param password 密码
+     * @return 返回该学生对象
+     */
+    Student login (String number, String password);
 }
