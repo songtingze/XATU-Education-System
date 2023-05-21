af0f0a3eb7ac7022a3c9d5e551e766aed6ab2d67
Chen760 <jzqcn2019@163.com>
2023-05-22 00:10:05 +0800

    文件上传、下载实现

--- xatu-homework/src/main/java/com/xatu/homework/service/HomeworkService.java
+++ xatu-homework/src/main/java/com/xatu/homework/service/HomeworkService.java
@@ -2,11 +2,16 @@ package com.xatu.homework.service;
 
 import com.xatu.common.domain.PageResult;
 import com.xatu.common.domain.Result;
-import com.xatu.homework.domain.Homework;
+import com.xatu.homework.domain.Attachment;
 import com.xatu.homework.domain.Student;
 import com.xatu.homework.domain.vo.CourseVO;
 import com.xatu.homework.domain.Course;
 import com.xatu.homework.domain.vo.TeacherVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public interface HomeworkService {
 
@@ -38,6 +43,12 @@ public interface HomeworkService {
      * 获取选取课程的全部学生
      */
     PageResult<Student> getStudentList(String courseNumber, Integer courseIndex,Integer current,Integer pageSize);
+    /**
+     * 附件上传
+     */
+    Result upload(MultipartFile file) throws IOException;
+
+    Result download(ServletOutputStream outputStream, String fileIndex) throws IOException;
 
 //    /**
 //     * 获取学生已选课程
